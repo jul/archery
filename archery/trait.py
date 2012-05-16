e9eb5459bf69eaf4b2e4903d9578d15250675225
julien tayon <jtayon@gmail.com>
2012-05-16 23:18:39 +0200

    adding subber & muler (subber dont word without muler)

--- archery/trait.py
+++ archery/trait.py
@@ -5,7 +5,7 @@
 You'll make your code perspire smartness by all its pore(c)(tm)(r).
 """
 from numbers import Number
-__all__ = [ 'Adder', 'Subber' ]
+__all__ = [ 'Adder', 'Subber', 'Muler' ]
 
 class Adder():
     """ making dict able to add 
@@ -19,11 +19,10 @@ class Adder():
  >>> toto = dad( int, dict(a = 1 ) )
  >>> print toto+tata
  defaultdict(<type 'int'>, {'a': 2, 'c': -1, 'b': 0})
- >>> toto-=( tata+tata )
- >>> print toto
- defaultdict(<type 'int'>, {'a': -1, 'c': 2, 'b': 0})
-"""
+ >>> toto+=1
+ defaultdict(<type 'int'>, {'a': 2}
 
+"""
     def __add__(self, other):
         """adder"""
         copy = self.copy()
@@ -46,8 +45,64 @@ class Adder():
         return self
     
     def __radd__(self, other):
+        copy=self.copy()
+        if isinstance(other, Number):
+            return copy.__iinc__(other)
+        return copy.__iadd__(other)
+
+
+class Muler():
+    """Making dict able to multiply"""
+
+    def __mul__(self, other):
+        """muler"""
+        copy = self.copy()
+        copy *= other
+        return copy
+
+    def __iscalmul__(self,number):
+        """in place increment"""
+        for k,v in self.iteritems():
+            self[k] *= number
+        return self
+        
+    def __imul__(self, other):
+        if isinstance(other,Number):
+            self.__iscalmul__(other)
+            return self
+        for k in other:
+            if k  in self:
+                self[k] = other[k]*self[k] if k in self else other[k]
+        return self
+    
+    def __rmul__(self, other):
         if isinstance(other, Number):
             copy=self.copy()
-            return copy.__iinc__(other)
-        return self.__add__(other)
+            return copy.__iscalmul__(other)
+        return self.__mul__(other)
 
+class Subber():
+    def __sub__(self, other):
+        """suber"""
+        copy = self.copy()
+        copy -= other
+        return copy
+
+    def __isub__(self, other):
+        if isinstance(other,Number):
+            self.__iinc__(-other)
+            return self
+            
+        for k,v in other.items() :
+            self[k] = self[k]-v  if k in self else -v
+        return self
+    
+    def __rsub__(self, other):
+        if isinstance(other, Number):
+            copy=self.copy()
+            return copy.__iinc__(-other)
+        return self.__sub__(other)
+    
+    def __neg__(self):
+        for k,v in self.iteritems():
+            self[k]*=-1
