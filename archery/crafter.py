6e3307cab92ee458dd86e94d4f7e4347bfc87307
julien tayon <jtayon@gmail.com>
2012-05-17 22:12:58 +0200

    typo

--- archery/crafter.py
+++ archery/crafter.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Soon will come sets of consistent adders/subber/muler/diver
-grouped in a consistent set known as algebrae
-all the combined traits will ensure consistant properties on :
-- commutativity;
-- associativity;
-- distributivity;
-- neutral element of addition & multiplication
-
-Planned : 
-Linear algebrae,
-Hermitian Algebrae 
-and so much more....
+"""
+Traits / mixins are the heart of archery, but sometimes you need functions to help you
 
 """
 from collections import Mapping
 
 def bowyer(_mapping_fact, _mapping_to_convert):
+    """the craftsman that makes bow
+    A function that given a function in the form 
+    f(tree) will convert a one level tree in your mapping"""
     _toc = _mapping_fact(_mapping_to_convert)
     for k,v in _toc.iteritems():
         if isinstance(v,Mapping):
-            _toc[k] = encoche( _mapping_fact, v)
+            _toc[k] = bowyer( _mapping_fact, v)
     return _toc
 
 def fletcher(tree, path=list()):
+    """The crastsman that fletches arrows. (poor idea I know)
+    iterator on a tree that yield an iterator on a mapping in the form of 
+    a list of ordered key that leads to the element and the value"""
+
     for k,v in tree.iteritems():
         if isinstance(v,Mapping):
             for child in fletcher(v, (path + [ k ]) ):
                 yield child
-
         else:
             yield   path + [k ,  v ] 
     
