451ed4dbd987f1f68db11260407ac0d8f06efa92
julien tayon <jtayon@gmail.com>
2012-05-23 18:19:16 +0200

    modif test for intricated mapping

--- test_encoche.py
+++ test_encoche.py
@@ -10,14 +10,16 @@ bowyer(Hankyu,{ 1 :  2, 3 : { 1 : 3 , 4 : { 5 : 1 }}})
 a = bowyer(Hankyu,{ 1 :  2, 3 : { 1 : 3 , 4 : { 5 : 1 }}})
 print a
 print a[3] + Hankyu( { 1 : 3 } ) 
-c= Hankyu({'foo': ['bar', 'baz']}) + Hankyu({'foo': ['bar', 'baz']})
+c= Hankyu({'foo': ['bar', 'baz']})
+d=c+c
 a+=1
 c["foo"][0]="bo"
-print c
+print d
 print a
-a = Hankyu({ 1 :  [2], 3 : { 1 : 3 , 4 : { 5 : 1 }}})
+a = bowyer(Hankyu,{ 1 :  [2], 3 : { 1 : 3 , 4 : { 5 : 1 }}})
 #a = bowyer(Hankyu,{ 1 :  [2], 3 : { 1 : 3 , 4 : { 5 : 1 }}})
 print a+{1:[3]}
-print a+{1:[3]}
+a+=bowyer(Hankyu,{1:[3], 3: { ("toto",) :  "tata" }})
+print a+a
 print Daikyu( toto="toto") * 0
 # OUT: defaultdict(<type 'int'>, {1: 3, 3: defaultdict(<type 'int'>, {1: 4, 4: defaultdict(<type 'int'>, {5: 2})})})
