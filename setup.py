2f77e44b1ba9ee38aef8a218da885e36ea5af7bf
julien tayon <jtayon@gmail.com>
2012-07-18 17:00:39 +0200

    adding test that block install

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-#!/usr/bin/env pyrhon
+#!/usr/bin/env python
 # -*- coding: "utf-8" -*-
 
 from distutils.command.build_py import build_py as _build_py
 from distutils.core import setup
+import unittest
+import sys
 
 def test():
     """Specialized Python source builder."""
+    loader= unittest.TestLoader()
+    suite=loader.discover(".", "test_archery.py")
+    runner=unittest.TextTestRunner()
+    result=runner.run(suite)
+    if  not result.wasSuccessful():
+        raise Exception( "Aborting install")
+
+
+if "install" in sys.argv:
+    test()
+
 setup(
         name='archery',
-        version='0.1.1',
+        version='0.1.2',
         author='Julien Tayon',
         author_email='julien@tayon.net',
         packages=['archery'],
